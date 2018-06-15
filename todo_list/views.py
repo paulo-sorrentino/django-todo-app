28bdf4d775e161f977371a22b71b236a29d8e132
paulo-sorrentino <inshideru@gmail.com>
2018-06-15 17:18:33 -0300

    Delete and messages

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,4 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from .models import List
+from .forms import ListForm
 
 def home(request):
-    return render(request, 'home.html',  {})
+    
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            all_items = List.objects.all
+            messages.success(request, 'Item has been added to list!')
+            return render(request, 'home.html', {'all_items': all_items})
+    else:
+        all_items = List.objects.all
+        return render(request, 'home.html', {'all_items': all_items})
+
+def about(request):
+    context = {'first_name': 'John', 'last_name': 'Elder'}
+    return render(request, 'about.html', context)
+
+def delete(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.delete()
+    messages.warning(request, 'Item has been deleted!')
+    return redirect('home')
